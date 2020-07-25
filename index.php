939f222ef5b5658445a7f89fc7c10e926fd3914d
Sagar-code-14 <60517287+Sagar-code-14@users.noreply.github.com>
2020-07-25 15:40:52 +0530

    meta added in index page
    meta description is very imp for google rank

--- index.php
+++ index.php
@@ -4,21 +4,19 @@
 
 <!-- index-515:19-->
 <head>
-   <!-- Basic Page Needs ================================================== -->
    <meta charset="utf-8">
-
-   <!-- Mobile Specific Metas ================================================== -->
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="viewport" content="width=device-width, user-scalable=no">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="title" content="Sri Gaanapriya music Academy">
+<meta name="description" content="Sri Gaanapriya music Academy aims to provide quality musical training to all age groups and Online music classes.">
+<meta name="keywords" content="music classes, Online music classes, Online music academy">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="revisit-after" content="15 days">
+<meta name="author" content="We Digitize U">
 
-   <!-- Site Title -->
-   <title>SRI GAANAPRIYA MUSIC ACADEMY &amp; GAANAPRIYA</title>
-
-
-
-   <!-- CSS
-         ================================================== -->
    <!-- Bootstrap -->
    <link rel="stylesheet" href="css/bootstrap.min.css">
 
@@ -38,13 +36,6 @@
    <link rel="stylesheet" href="css/style.css">
    <!-- Responsive styles-->
    <link rel="stylesheet" href="css/responsive.css">
-
-   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-   <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script >
